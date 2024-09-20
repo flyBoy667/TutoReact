3e04945d0b507e56ca42c9e3c20580aa4fa9ef5e
flyBoy667 <salifoudembele100@outlook.com>
2024-09-20 19:55:23 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+const title = "Hello world"
+const styles = {
+    backgroundColor: 'blue',
+    color: 'red'
+}
+const handleClick = (e) => {
+    alert("Hello world")
+    console.log(e)
+}
+
 function App() {
-    return <h1>Hello World</h1>
+    return <>
+        <h1 onClick={handleClick} style={styles} >{title}</h1>
+        <input type="text"/>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+    </>
 }
 
 export default App
