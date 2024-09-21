329a47e3e694fa3e126c445fd5f95ff7d6473659
flyBoy667 <salifoudembele100@outlook.com>
2024-09-21 19:51:35 +0000

    Form event

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import {useState} from "react";
 
 function App() {
-    const [name, setName] = useState('John Smith');
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    }
-
-    const reset = () => {
-        setName('');
-    }
     console.log('render')
+    const [text, setText] = useState('');
+    const [checked, setChecked] = useState(true);
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+    const toggleChecked = () => {
+        setChecked(!checked);
+    }
+
     return <form>
-        <label>
-            Name:
-            <input type="text" name="name" value={name} onChange={handleNameChange}/>
-        </label>
-        <input type="reset" value="Reset" onClick={reset}/>
+        <textarea value={text} onChange={handleChange}/>
+        <input type="checkbox" checked={checked} onChange={toggleChecked}/>
+        <input type="submit" disabled={!checked} value="Envoyer"/>
     </form>
 
 }
