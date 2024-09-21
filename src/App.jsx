93e6eb948904c7e8949869f2b736f46080f1d2e1
flyBoy667 <salifoudembele100@outlook.com>
2024-09-21 19:07:59 +0000

    first hook : useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,33 @@
-const title = "Hello world";
-const styles = {
-  backgroundColor: "blue",
-  color: "red",
-};
-
-const todos = ["First todo", "Second todo", "Third todo"];
-
-const showTitle = true;
-
-const handleClick = (e) => {
-  alert("Hello world");
-  console.log(e);
-};
+import {useState} from "react";
 
 function App() {
-  return (
-    <>
-      {showTitle ? (
-        <Title color="green">{title}</Title>
-      ) : (
-        <p>Welcome to my website</p>
-      )}
-      <input type="text" />
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}</li>
-        ))}
-      </ul>
-    </>
-  );
-}
+    const [person, setPerson] = useState(
+        {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 18
+        }
+    )
 
-function Title({ color, hidden, children }) {
-  return hidden ? null : <h1 style={{ color }}>{children}</h1>;
+    const [count, setCount] = useState(0)
+
+    const incrementAge = () => {
+        setPerson({...person, age: person.age + 1})
+    }
+
+    const incrementCount = () => {
+        setCount(c => c + 1)
+        setCount(c => c + 1)
+        setCount(c => c + 1)
+    }
+
+    return <>
+        <p>Age de {person.firstName} : {person.age}</p>
+        <button onClick={incrementAge}>Gagner une annee</button>
+        <p>Compteur : {count}</p>
+        <button onClick={incrementCount}>Incrementer le compteur de trois</button>
+    </>
+
 }
 
 export default App;
